0219f56e64d5efded8ee66aa0ed959a7d5e7eee8
Ganesh Parmar <80880253+GaneshParmar@users.noreply.github.com>
2021-10-09 07:53:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+if(screen.availWidth>600){
+    // location.reload();   
+    document.getElementById("body").innerHTML="<h1 style='color:red;'>Sorry Currently only for small screen Mobiles</h1>";
+}
