b361247fdd818049309aa65ece888b187b36fa18
Ganesh Parmar <80880253+GaneshParmar@users.noreply.github.com>
2021-11-14 12:56:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 if(screen.availWidth>600){
     // location.reload();   
-    document.getElementById("body").innerHTML="<h1 style='color:red;'>Sorry Currently only for small screen Mobiles</h1>";
+    document.getElementById("body").innerHTML="<h1 style='color:red;'>Sorry Currently only for small screen Mobiles</h1>\
+    <br><p style='color:white;'>If you are phone reload the website <a href='#' onclick='reload()'>here</a></p>\
+       ";
+}
+// Website reload
+function reload() {
+    location.reload();
 }
